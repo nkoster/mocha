fc163465f2459ffeb02a01e8c000901191fcf5c8
Niels Koster <n.koster@portavita.eu>
2020-03-05 23:28:57 +0100

    Initial, mocha test example

---
+++ js/sum.js
@@ -0,0 +1,9 @@
+module.exports = _ => {
+    var args = Array.prototype.slice.call(arguments)
+    if (!args.every(Number.isFinite)) {
+      throw new TypeError('sum() expects only numbers.')
+    }
+    return args.reduce(function(a, b) {
+      return a + b
+    }, 0)
+}
