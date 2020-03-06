802f879a87eb5918a4bc04f6df5ed4a61230e1fe
Niels Koster <n.koster@portavita.eu>
2020-03-07 00:34:49 +0100

    Only run on Array

--- js/flat-array.js
+++ js/flat-array.js
@@ -1,14 +1,18 @@
-module.exports = a => {
-    let t = []
-    const flat = item => {
-        if (item.constructor === Array) {
-            for (let i = 0; i < item.length; i++) {
-                flat(item[i])
+module.exports = arr => {
+    if (arr.constructor === Array) {
+        let newArr = []
+        const flat = item => {
+            if (item.constructor === Array) {
+                for (let i = 0; i < item.length; i++) {
+                    flat(item[i])
+                }
+            } else {
+                newArr.push(item)
             }
-        } else {
-            t.push(item)
         }
+        flat(arr)
+        return newArr
+    } else {
+        return arr
     }
-    flat(a)
-    return t
 }
