e640d37e41d9fec521c3eb2253fd90b4c85e46f3
Niels Koster <n.koster@portavita.eu>
2020-03-07 00:18:33 +0100

    Pass test

--- test/aap.js
+++ test/aap.js
@@ -2,7 +2,7 @@ const
     aap = require('../js/aap'),
     expect = require('chai').expect
 
-describe('#aap()', _ => {
+describe('aap()', _ => {
     context('aap test', _ => {
         it('should return aap', done => {
             expect(aap()).to.equal('aap')
