19369833385d48dfca7bce54191e9f1d24146e5b
Niels Koster <n.koster@portavita.eu>
2020-03-07 00:19:15 +0100

    Array flatten function test

---
+++ test/flat-array.js
@@ -0,0 +1,12 @@
+const
+    flatArray = require('../js/flat-array'),
+    expect = require('chai').expect
+
+describe('flatArray()', _ => {
+    context('flat array test', _ => {
+        it('should return a flat array', done => {
+            expect(flatArray([[1,2],[3,4]])).to.eql([1,2,3,4])
+            done()
+        })
+    })
+})
